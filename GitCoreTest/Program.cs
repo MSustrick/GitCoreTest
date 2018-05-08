e6430e8d05c4c70a17bd54594b7c5bc28087e69d
Msustrick <msustrick@gmail.com>
2018-05-08 17:05:38 -0500

    no message

--- GitCoreTest/Program.cs
+++ GitCoreTest/Program.cs
@@ -9,7 +9,6 @@ namespace GitCoreTest
             Console.WriteLine("Hello World!");
             Console.WriteLine("Master Check in 1");
             Console.WriteLine("Branch ! Check in");
-            Console.WriteLine("Branch 1 Check in 2");
             Console.ReadLine();
         }
     }

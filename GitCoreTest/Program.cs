fd103352224629f4c1cac7f890842ddddbd085fc
Msustrick <msustrick@gmail.com>
2018-05-08 16:45:49 -0500

    Add project files.

---
+++ GitCoreTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
